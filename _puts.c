7c3e7061a976dc6a91522f17ace6d53075cac798
karimsamiir <karimsamir010r@gmail.com>
2023-07-25 05:36:57 -0700

    kariim

--- _puts.c
+++ _puts.c
@@ -2,19 +2,23 @@
 
 /**
 * _puts - prints str with newline
-* @str: str to print
+* @str: str tp print
 * Return: void
 */
-void _puts(char *str)
+int _puts(char *str)
 {
+	char *s = str;
+
 	while (*str)
 		_putchar(*str++);
+	return (str - s);
 }
 
 /**
 * _putchar - print char c to stdout
 * @c: the char to print
-* Return: 1 on success, -1 on error
+* Return: 1 on success
+* on error, -1
 */
 int _putchar(int c)
 {
@@ -26,8 +30,7 @@ int _putchar(int c)
 		write(1, buf, i);
 		i = 0;
 	}
-	if (c != BUF_FLUSH)
+	if ( c != BUF_FLUSH)
 		buf[i++] = c;
-	return (c != BUF_FLUSH ? 1 : -1);
+	return (1);
 }
-
